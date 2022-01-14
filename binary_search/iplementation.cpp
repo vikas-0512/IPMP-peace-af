5db036410322261181e59fc12e7d041cde1fe35a
vikas-0512 <83655845+vikas-0512@users.noreply.github.com>
2022-01-14 19:05:32 +0530

    Create iplementation.cpp

---
+++ binary_search/iplementation.cpp
@@ -0,0 +1,30 @@
+# include <iostream>
+using namespace std;
+void binarysearch(int arr[],int n,int key)
+{
+    int s=0,e=n-1,mid=(n-1)/2;
+    while(s<=e)
+    {
+        cout<<"hello"<<endl;
+        if(arr[mid]==key)
+        {
+            cout<<"element found"<<endl;
+            return;
+        }
+        else if(arr[mid]>key)
+        {
+            e=mid-1;
+        }
+        else if(arr[mid]<key)
+        {
+            s=mid+1;
+        }
+    }
+    cout<<"element not found"<<endl;
+    return;
+}
+int main()
+{
+    int arr[]={1,2,4,7,8};
+    binarysearch(arr,5,9);
+}
