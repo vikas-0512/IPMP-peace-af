b11dba19efbc16065dda66b7064dcbf1df5a53e4
vikas-0512 <83655845+vikas-0512@users.noreply.github.com>
2022-01-22 16:36:01 +0530

    Create ceiling_of_element_in_a_sorted_array.cpp

---
+++ binary_search/ceiling_of_element_in_a_sorted_array.cpp
@@ -0,0 +1,20 @@
+ int findCeling(long long int arr[], int N, long long int K) {
+    int start=0,end=N-1,mid,res=N+1;
+    while(start<=end)
+    {
+        mid=start+(end-start)/2;
+        if(arr[mid]==K)
+          return mid;
+        else if(arr[mid]>K)
+        {
+            res=mid;
+            end=mid-1;
+        }
+        else if(arr[mid]<K)
+        {
+          
+         start=mid+1;
+        }
+    }
+    return res;  
+}
