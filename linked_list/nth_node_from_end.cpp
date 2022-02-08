63052e0c67bd12b3041dcf34eb2093aebc59cf85
vikas-0512 <83655845+vikas-0512@users.noreply.github.com>
2022-02-08 12:23:06 +0530

    Update nth_node_from_end.cpp

--- linked_list/nth_node_from_end.cpp
+++ linked_list/nth_node_from_end.cpp
@@ -19,3 +19,20 @@ int getNthFromLast(struct Node *head, int n)
         return p->data;
     }
 }
+//using double pointers
+int getNthFromLast(struct Node *head, int n)
+{
+    struct Node *p=head,*q=head;
+    for(int i=0;i<n;i++)
+    {
+        if(p==NULL)
+          return -1;
+        p=p->next;
+    }
+    while(p!=NULL)
+    {
+        q=q->next;
+        p=p->next;
+    }
+    return q->data;
+}
