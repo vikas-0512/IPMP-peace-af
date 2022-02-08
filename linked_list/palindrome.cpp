fd055dbf9838ea5f1b50e5f5c123599fbcfa859b
vikas-0512 <83655845+vikas-0512@users.noreply.github.com>
2022-02-08 14:25:28 +0530

    Update palindrome.cpp

--- linked_list/palindrome.cpp
+++ linked_list/palindrome.cpp
@@ -21,3 +21,48 @@ class Solution{
           return true;
     }
 };
+class Solution{
+  public:
+    struct Node *reverse(struct Node *head)
+    {
+        if(!head || !head->next)
+          return head;
+        else
+        {
+            struct Node *newhead=reverse(head->next);
+            head->next->next=head;
+            head->next=NULL;
+            return newhead;
+        }
+    }
+    bool isPalindrome(Node *head)
+    {
+        struct Node *ori=NULL,*end;
+        struct Node *rev,*p=head;
+        while(p!=NULL)
+        { 
+            struct Node *temp=new Node(p->data);
+            if(ori==NULL)
+            {
+                ori=temp;
+                end=temp;
+            }
+            else
+            {
+                end->next=temp;
+                end=end->next;
+            }
+            p=p->next;
+        }
+        rev=reverse(head);
+        while(ori || rev)
+        {
+            if(ori->data != rev->data)
+              return false;
+            ori=ori->next;
+            rev=rev->next;
+            
+        }
+        return true;
+    }
+};
